3a140e7a5416c8b6f56a7d3d7e9d59a71e039ee4
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-14 06:51:12 +0000

    corrected error message

--- mul_func.c
+++ mul_func.c
@@ -13,7 +13,7 @@ void mul_func(stack_t **stack, unsigned int line_number)
 
 	if ((*stack) == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n",
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
 			line_number);
 		fclose(global.fd);
 		free(global.opcode);
